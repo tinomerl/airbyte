a6cb74ff625f19747f364f98430fc8ad65a2f777
tinomerl <info@tinomerl.de>
2022-11-07 16:42:30 +0100

    Added Incremental Class and updated methods to handle pagination and incremental Calls

--- airbyte-integrations/connectors/source-linkedin-pages/source_linkedin_pages/source.py
+++ airbyte-integrations/connectors/source-linkedin-pages/source_linkedin_pages/source.py
@@ -4,13 +4,15 @@
 
 
 from abc import ABC
+from datetime import datetime
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
+import pendulum
 import requests
 from airbyte_cdk import AirbyteLogger
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
-from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams import Stream, IncrementalMixin
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator, TokenAuthenticator
 
@@ -18,11 +20,12 @@ from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator, TokenAuth
 class LinkedinPagesStream(HttpStream, ABC):
 
     url_base = "https://api.linkedin.com/v2/"
-    primary_key = None
+    primary_key = ""
 
-    def __init__(self, config):
+    def __init__(self, config, **kwargs):
         super().__init__(authenticator=config.get("authenticator"))
         self.config = config
+
 
     @property
     def org(self):
@@ -34,8 +37,19 @@ class LinkedinPagesStream(HttpStream, ABC):
         return self.endpoint
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        content = response.json()
+        paging = content.get("paging")
+        if paging and paging.get("links"):
+            offset = paging["start"] + paging["count"]
+            return {"start": offset, "count": paging["count"]}
         return None
 
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, stream_slice, next_page_token)
+        if next_page_token: 
+            params.update(next_page_token)
+        return params
+    
     def parse_response(
         self, response: requests.Response, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None
     ) -> Iterable[Mapping]:
@@ -53,15 +67,60 @@ class LinkedinPagesStream(HttpStream, ABC):
             self.logger.error(error_message)
         return super().should_retry(response)
 
+class LinkedinPagesIncremental(LinkedinPagesStream, IncrementalMixin):
+    cursor_field = "start"
+    primary_key = "start"
+
+    def __init__(self, config, start_date: datetime, **kwargs):
+        super().__init__(config=config)
+        self.start_date = start_date
+        self._cursor_value = None
+
+    @property
+    def state(self) -> Mapping[str, Any]:
+        if self._cursor_value:
+            return {self.cursor_field: self._cursor_value.strftime('%Y-%m-%d')}
+        elif self.start_date:
+            return {self.cursor_field: self.start_date.strftime("%Y-%m-%d")}
+    
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, stream_slice, next_page_token)
+        if self.state:
+            params.update({
+                "timeIntervals.timeGranularityType": "DAY",
+                "timeIntervals.timeRange.start": pendulum.from_format(self.state[self.cursor_field], "YYYY-MM-DD").int_timestamp * 1000,
+                "timeIntervals.timeRange.end": datetime.now().timestamp() * 1000
+            })
+        return params
+
+    @state.setter
+    def state(self, value: Mapping[str, Any]):
+        self._cursor_value = pendulum.from_format(value[self.cursor_field], "YYYY-MM-DD")
+
+    def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
+        for record in super().read_records(*args, **kwargs):
+            # The time range for LinkedIn Pages is saved in a nested dict
+            # This way it is put in the main record and can be parsed easier.
+            time_range = record.get("timeRange")
+            if time_range:
+                record.update(time_range)
+                record.pop("timeRange")
+            if self._cursor_value:
+                latest_record_date = pendulum.from_timestamp(record[self.cursor_field]/1000)
+                self._cursor_value = max(self._cursor_value, latest_record_date)
+            yield record
+
+
 
 class OrganizationLookup(LinkedinPagesStream):
+
     def path(self, stream_state: Mapping[str, Any], **kwargs) -> MutableMapping[str, Any]:
 
         path = f"organizations/{self.org}"
         return path
 
 
-class FollowerStatistics(LinkedinPagesStream):
+class FollowerStatistics(LinkedinPagesIncremental):
     def path(self, stream_state: Mapping[str, Any], **kwargs) -> MutableMapping[str, Any]:
 
         path = f"organizationalEntityFollowerStatistics?q=organizationalEntity&organizationalEntity=urn:li:organization:{self.org}"
@@ -73,11 +132,21 @@ class FollowerStatistics(LinkedinPagesStream):
         yield from response.json().get("elements")
 
 
-class ShareStatistics(LinkedinPagesStream):
-    def path(self, stream_state: Mapping[str, Any], **kwargs) -> MutableMapping[str, Any]:
+class ShareStatistics(LinkedinPagesIncremental):
+    """https://learn.microsoft.com/en-us/linkedin/marketing/integrations/community-management/organizations/share-statistics
+    """
 
-        path = f"organizationalEntityShareStatistics?q=organizationalEntity&organizationalEntity=urn%3Ali%3Aorganization%3A{self.org}"
+    def path(self, stream_state: Mapping[str, Any], **kwargs) -> MutableMapping[str, Any]:
+        path = "organizationalEntityShareStatistics"
         return path
+
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
+        params = super().request_params(stream_state, stream_slice, next_page_token)
+        params.update({
+            "q": "organizationalEntity",
+            "organizationalEntity": f"urn%3Ali%3Aorganization%3A{self.org}"
+        })
+        return params
 
     def parse_response(
         self, response: requests.Response, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None
@@ -87,7 +156,6 @@ class ShareStatistics(LinkedinPagesStream):
 
 class TotalFollowerCount(LinkedinPagesStream):
     def path(self, stream_state: Mapping[str, Any], **kwargs) -> MutableMapping[str, Any]:
-
         path = f"networkSizes/urn:li:organization:{self.org}?edgeType=CompanyFollowedByMember"
         return path
 
@@ -144,4 +212,8 @@ class SourceLinkedinPages(AbstractSource):
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         config["authenticator"] = self.get_authenticator(config)
-        return [OrganizationLookup(config), FollowerStatistics(config), ShareStatistics(config), TotalFollowerCount(config)]
+        if config.get("start_date"):
+            start_date = pendulum.from_format(config['start_date'], 'YYYY-MM-DD')
+        else:
+            start_date = None
+        return [OrganizationLookup(config), FollowerStatistics(config, start_date), ShareStatistics(config, start_date), TotalFollowerCount(config)]
